be9ddb3f076d054f9ea6c215f7191695e84b83ce
krthr <wtovaragudelo@gmail.com>
2023-02-03 13:18:32 +0100

    remove unused package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,22 @@ export default defineNuxtConfig({
       bodyAttrs: {
         class: "bg-base-200",
       },
+
+      script: [
+        {
+          async: true,
+          src: "https://www.googletagmanager.com/gtag/js?id=G-K40DJ3THNZ",
+        },
+        {
+          innerHTML: `
+          window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-K40DJ3THNZ');
+          `,
+        },
+      ],
     },
   },
 
