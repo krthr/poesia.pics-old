e5b6923c3ffcaae7907161e15e078b03ea0ef7d5
krthr <wtovaragudelo@gmail.com>
2023-02-04 22:04:07 +0100

    feat: share component

--- components/App/Poem.vue
+++ components/App/Poem.vue
@@ -26,44 +26,12 @@
       <LazyAppPoemColors />
     </div>
 
-    <div class="mt-10 text-center w-full">
-      <button
-        :class="{
-          'btn btn-sm btn-ghost btn-outline gap-2': true,
-          loading: downloading,
-        }"
-        :disabled="downloading"
-        @click="downloadImage()"
-      >
-        <SvgIcon :path="mdiDownload" type="mdi" class="w-4 h-4" />
-        <span>Descargar imagen</span>
-      </button>
-    </div>
+    <LazyAppPoemShare />
   </div>
 </template>
 
 <script lang="ts" setup>
-import SvgIcon from "@jamescoyle/vue-icon";
-import { mdiDownload } from "@mdi/js";
 import { useAppStore } from "@/stores/appStore";
-import { logEvent } from "@/utils/gtag";
 
 const appStore = useAppStore();
-const downloading = ref(false);
-
-async function downloadImage() {
-  downloading.value = true;
-
-  try {
-    const { generateAndDownloadImage } = await import("@/utils/image");
-    const element = document.querySelector("#poem")! as HTMLElement;
-    await generateAndDownloadImage(element, appStore.result.poem);
-
-    logEvent("share");
-  } catch (error) {
-    console.error(error);
-  }
-
-  downloading.value = false;
-}
 </script>
