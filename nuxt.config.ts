cd4fea5d40aad4ca8a5d67e2d413f177b39161f1
krthr <wtovaragudelo@gmail.com>
2023-02-05 15:54:44 +0100

    feat: buyme a coffee btn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,28 +8,7 @@ gtag('js', new Date());
 gtag('config', 'G-K40DJ3THNZ');
 `;
 
-const SCRIPTS: any[] = [
-  {
-    "data-name": "BMC-Widget",
-    "data-cfasync": "false",
-    "data-id": "wilsontovar",
-    "data-description": "Support me on Buy me a coffee!",
-    "data-message":
-      "¡Gracias por visitar! Con tu aporte podemos seguir llenando este mundo de poemas",
-    "data-color": "#FF5F5F",
-    "data-position": "Right",
-    "data-x_margin": "18",
-    "data-y_margin": "18",
-    src: "https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js",
-    tagPosition: "bodyClose",
-  },
-  {
-    async: true,
-    src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7162418263936911",
-    crossorigin: "anonymous",
-    tagPosition: "bodyClose",
-  },
-];
+const SCRIPTS: any[] = [];
 
 if (isProd) {
   SCRIPTS.push(
