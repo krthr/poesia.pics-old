a11527cc8ee4af9600ad061dbbeb88e45691de32
krthr <wtovaragudelo@gmail.com>
2023-02-11 00:54:17 +0100

    feat: get poem endpoint /api/poem/:id

---
+++ server/api/poems/[id].get.ts
@@ -0,0 +1,21 @@
+import { logger } from "@/server/logger";
+import { poemsCollection } from "@/server/services/Firebase";
+
+export default defineEventHandler(async (event) => {
+  const id = event.context.params.id;
+  const doc = await poemsCollection
+    .doc(id)
+    .get()
+    .catch((error) => {
+      logger.error(error);
+    });
+
+  if (!doc?.exists) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: "Poema no encontrado.",
+    });
+  }
+
+  return { id: doc.id, ...doc.data() };
+});
